bc99d3071cdf30e3bc77b7c368f7c2dbc809ab89
OzleSig <ozlemsigbeku@protonmail.com>
2024-09-29 19:55:54 +0100

    minor changes

--- animal_classes.py
+++ animal_classes.py
@@ -1,2 +1,11 @@
 class animal:
-    def __init__(self)
+    def __init__(self, type, colour, name):
+        self.type = type
+        self.colour = colour
+        self.name = name
+    
+    def make_noise():
+        pass
+
+    def info(name):
+        pass
